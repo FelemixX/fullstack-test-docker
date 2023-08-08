6eda6e4d2dcbcfe5b965fc4522c9fd34378830af
FelemixX <yuukiasunasanwa@gmail.com>
2023-08-08 18:31:59 +0300

    task-1 Добавил заголовки и ajax

--- src/app/Views/templates/main/header.php
+++ src/app/Views/templates/main/header.php
@@ -1,17 +1,18 @@
-<?php ini_set('display_errors', 1) ?>
 <!doctype html>
 <html>
 <head>
-    <title>Заголовок</title>
+    <title><?= $title ?? 'Заголовок по умолчанию' ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body>
 <div class="container">
     <div class="row justify-content-center mt-5">
         <div class="col-4 col-sm-5 col-md-6 col-lg-6 col-xl-6">
-            <h1 class="text-center">Заголовок поменьше</h1>
+            <h1 class="text-center"><?= $title ?? 'Заголовок по умолчанию' ?></h1>
         </div>
         <div class="w-100"></div>
